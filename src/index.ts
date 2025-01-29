6a7093c8075bf8bbe5d418a5469b537b9996daf8
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2025-01-29 14:27:14 +0800

    Use vite

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(_) {
-    return new Response("Bun!");
-  },
-  development: true,
-});
-
-console.log(`Listening on http://localhost:${server.port} ...`);

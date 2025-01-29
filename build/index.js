d69987e1509d30fd731de0e8efea16d92f223158
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2025-01-29 10:15:45 +0800

    remove build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.ts
-var server = Bun.serve({
-  port: 3000,
-  fetch(_) {
-    return new Response("Bun!");
-  },
-  development: true
-});
-console.log(`Listening on http://localhost:${server.port} ...`);
